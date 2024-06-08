d7c128727c9b80118a0f9d960e522fda0d855659
nandinipanchal001 <nandini_panchal@apprely.com>
2024-06-08 14:15:59 +0530

    pagination added

--- src/pages/customer/CustomerTable.jsx
+++ src/pages/customer/CustomerTable.jsx
@@ -1,6 +1,6 @@
 import _ from "lodash";
 import React, { useState, useEffect } from "react";
-import { Table, Input } from "antd";
+import { Table, Pagination } from "antd";
 import { useNavigate } from "react-router-dom";
 import getCustomers from "../services/customerService";
 import { useQuery } from "react-query";
@@ -15,13 +15,20 @@ import { IoMdCheckmarkCircle } from "react-icons/io";
 import { IoMdCloseCircle } from "react-icons/io";
 
 const CustomerTable = () => {
-  let { cgid, name, mobile, email,recordStatus } = QueryParams();
+  let { pageNo, cgid, name, mobile, email, recordStatus } = QueryParams();
   const navigateTo = useNavigate();
-  const [filters, setFilters] = useState({ pageNo: 1, pageSize: 50 });
-  const [values, setValues] = useState({ name, cgid, mobile, email,recordStatus });
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+  const [filters, setFilters] = useState({ pageNo: pageNo || 1, pageSize: rowsPerPage });
+  const [values, setValues] = useState({
+    name,
+    cgid,
+    mobile,
+    email,
+    recordStatus,
+  });
 
   const { data } = useQuery(
-    ["customer", cgid, name, mobile, email,recordStatus, filters],
+    ["customer", cgid, name, mobile, email, recordStatus, filters],
     () => getCustomers(filters)
   );
 
@@ -36,7 +43,7 @@ const CustomerTable = () => {
         name,
         mobile,
         email,
-        recordStatus
+        recordStatus,
       };
       const newParams = removeDuplicates(params);
       setFilters({
@@ -44,14 +51,37 @@ const CustomerTable = () => {
         ...newParams,
       });
     }
-  }, [cgid, name, mobile, email,recordStatus]);
-
-  console.log("useparams", cgid, name, mobile, email, filters);
+  }, [cgid, name, mobile, email, recordStatus]);
 
   const customers = _.get(data, "customers") || [];
-  //   const count = _.get(data, "count");
+  const count = _.get(data, "count");
 
-  //   console.log("pageSize", filters,values);
+  useEffect(() => {
+    if (!pageNo) {
+      pageNo = 1;
+      handlePageChange({ current: pageNo, pageSize: rowsPerPage });
+    }
+  }, [pageNo]);
+
+  const handlePageChange = (updatePage) => {
+    if (!_.isEmpty(updatePage)) {
+      let params = {
+        pageNo: updatePage ? updatePage.current : 1,
+      };
+      navigateTo(FilterUtils.createUrl(params));
+
+      console.log('updatePage',updatePage)
+      if (updatePage.pageSize !== rowsPerPage || updatePage.current !== pageNo) {
+        setRowsPerPage(updatePage.pageSize);
+        setFilters({
+          ...filters,
+          pageNo:updatePage.current,
+          pageSize:updatePage.pageSize
+        })
+      }
+    }
+  };
+    console.log("filters", filters,rowsPerPage,pageNo);
 
   const onReset = () => {
     setFilters({ pageNo: 1, pageSize: 50 });
@@ -116,7 +146,9 @@ const CustomerTable = () => {
       <div className="px-10">
         <div className="flex flex-row justify-between pb-12">
           <h3 className="text-3xl">Customer</h3>
-          <div className="text-lg order-last bg-[#ffed00] py-0.5 px-4 text-purple-800">+ Create</div>
+          <div className="text-lg order-last bg-[#ffed00] py-0.5 px-4 text-purple-800">
+            + Create
+          </div>
         </div>
         <div className="flex flex-row p-3 bg-[#ffffff] justify-between">
           <div className="flex flex-row gap-3">
@@ -154,19 +186,28 @@ const CustomerTable = () => {
               filterValue={"email"}
             />
             <FilterDropDown
-            values={values}
-            setValues={setValues}
-            placeholder={"Status"}
-            filterValue={"recordStatus"}
+              values={values}
+              setValues={setValues}
+              placeholder={"Status"}
+              filterValue={"recordStatus"}
             />
-
           </div>
           <div className="flex flex-row order-last gap-6">
             <IoSearch size={24} onClick={() => onFilter()} />
             <VscRefresh size={24} onClick={() => onReset()} />
           </div>
         </div>
-        <Table columns={columns} dataSource={customers} />
+        <Table columns={columns} dataSource={customers} pagination={false}/>
+        <Pagination
+          total={count}
+          current={pageNo}
+          pageSize={rowsPerPage}
+          showSizeChanger={true}
+          showTotal={(total, range) => `Total: ${total}`}
+          onChange={(page, pageSize) => {
+            handlePageChange({ current: page, pageSize: pageSize });
+          }}
+        />
       </div>
     </div>
   );
